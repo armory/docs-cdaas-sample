dfa54b143405771f7688e83b02dde45ca5a582db
Aimee Ukasick <aimeeu.opensource@gmail.com>
2022-04-26 15:15:30 -0500

    remove infosec env; remove 'borealis' in setup.sh

--- configuration/setup.sh
+++ configuration/setup.sh
@@ -1,19 +1,20 @@
 echo "reminder: run configuration/setup.sh <clientID> <clientSecret> where clientID and clientSecret are the credentials from step 2."
-echo "setting up borealis agents using clientIt: $1 and secret: $2"
-kubectl create namespace borealis-demo-agent-prod
-kubectl create namespace borealis-demo-agent-staging
-kubectl create namespace borealis-demo-agent-dev
-kubectl create ns borealis-demo-infra
-kubectl create ns borealis-dev
-kubectl create ns borealis-staging
-kubectl create ns borealis-infosec
-kubectl create ns borealis-prod
-kubectl create ns borealis-prod-eu
-kubectl create ns borealis-demo-agent-prod-eu
-kubectl -n=borealis-demo-agent-prod create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
-kubectl -n=borealis-demo-agent-staging create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
-kubectl -n=borealis-demo-agent-dev create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
-kubectl -n=borealis-demo-agent-prod-eu create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+echo "setting up remote network agents using clientIt: $1 and secret: $2"
+kubectl create namespace demo-agent-prod
+kubectl create namespace demo-agent-staging
+kubectl create namespace demo-agent-dev
+kubectl create namespace demo-agent-prod-eu
+kubectl create ns demo-infra
+kubectl create ns demo-dev
+kubectl create ns demo-staging
+kubectl create ns demo-prod
+kubectl create ns demo-prod-eu
+
+kubectl -n=demo-agent-prod create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=demo-agent-prod-eu create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=demo-agent-staging create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=demo-agent-dev create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+
 
 # Optionally Add Armory Chart repo, if you haven't
 helm repo add armory https://armory.jfrog.io/artifactory/charts
@@ -25,28 +26,28 @@ helm upgrade --install armory-rna-prod armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
     --set agentIdentifier=demo-prod-west-cluster \
-    -n borealis-demo-agent-prod
-helm upgrade --install armory-rna-staging armory/remote-network-agent \
-    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
-    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=demo-staging-cluster \
-    -n borealis-demo-agent-staging
-helm upgrade --install armory-rna-dev armory/remote-network-agent \
-    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
-    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=demo-dev-cluster \
-    -n borealis-demo-agent-dev
+    -n demo-agent-prod
 helm upgrade --install armory-rna-prod-eu armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
     --set agentIdentifier=demo-prod-eu-cluster \
-    -n borealis-demo-agent-prod-eu
-#helm install prometheus prometheus-community/kube-prometheus-stack -n=borealis-demo-infra --set kube-state-metrics.metricLabelsAllowlist[0]=pods=[*]
-helm install prometheus prometheus-community/kube-prometheus-stack -n=borealis-demo-infra --set "kube-state-metrics.metricAnnotationsAllowList[0]=pods=[*]" --set "global.scrape_interval=5s"
+    -n demo-agent-prod-eu
+helm upgrade --install armory-rna-staging armory/remote-network-agent \
+    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
+    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
+    --set agentIdentifier=demo-staging-cluster \
+    -n demo-agent-staging
+helm upgrade --install armory-rna-dev armory/remote-network-agent \
+    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
+    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
+    --set agentIdentifier=demo-dev-cluster \
+    -n demo-agent-dev
+
+helm install prometheus prometheus-community/kube-prometheus-stack -n=demo-infra --set "kube-state-metrics.metricAnnotationsAllowList[0]=pods=[*]" --set "global.scrape_interval=5s"
 
 BASEDIR=$(dirname $0)
 
-kubectl apply -f "$BASEDIR/../manifests/potato-facts-external-service.yml" -n borealis-prod-eu #Temporary workaround for YODL-300. deploying service along side deployment does not work for Blue/Green.
+kubectl apply -f "$BASEDIR/../manifests/potato-facts-external-service.yml" -n demo-prod-eu #Temporary workaround for YODL-300. deploying service along side deployment does not work for Blue/Green.
 #container_cpu_load_average_10s{namespace="borealis", job="kubelet"} * on (pod)  group_left (label_app) sum(kube_pod_labels{job="kube-state-metrics",label_app="hostname",namespace="borealis"}) by (label_app, pod)
 
 # also tried: --set kube-state-metrics.metricLabelsAllowlist=pods=[*]
