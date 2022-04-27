5ef5dbf934a91f9410662d23a2b4ea82a6985330
Aimee Ukasick <aimeeu.opensource@gmail.com>
2022-04-27 16:29:45 -0500

    changed namespaces; cleaned up scripts

--- configuration/delete-demo-config.sh
+++ configuration/delete-demo-config.sh
@@ -1,13 +1,5 @@
-helm uninstall armory-rna-prod \
-    -n borealis-demo-agent-prod
-helm uninstall armory-rna-prod-eu \
-    -n borealis-demo-agent-prod-eu
-helm uninstall armory-rna-staging \
-    -n borealis-demo-agent-staging
-helm uninstall armory-rna-dev \
-    -n borealis-demo-agent-dev
-helm uninstall prometheus -n=borealis-demo-infra
-#kubectl delete namespace borealis-demo-agents
-#kubectl delete namespace borealis-demo-agent-prod
-#kubectl delete namespace borealis-demo-agent-staging
-#kubectl delete namespace borealis-demo-agent-dev
+helm uninstall demo-rna-prod-us  -n demo-agent-prod-us
+helm uninstall demo-rna-prod-eu -n demo-agent-prod-eu
+helm uninstall demo-rna-staging -n demo-agent-staging
+helm uninstall demo-rna-dev -n demo-agent-dev
+helm uninstall prometheus -n=demo-infra
--- configuration/setup.sh
+++ configuration/setup.sh
@@ -1,16 +1,21 @@
-echo "reminder: run configuration/setup.sh <clientID> <clientSecret> where clientID and clientSecret are the credentials from step 2."
-echo "setting up remote network agents using clientIt: $1 and secret: $2"
-kubectl create namespace demo-agent-prod
+#!/bin/bash
+if [ "$#" -ne 2 ]; then
+    echo "Format is setup.sh <clientID> <clientSecret> where clientID and clientSecret are the Borelais credentials for the demo app."
+    exit 1
+fi
+
+kubectl create namespace demo-agent-prod-us
+kubectl create namespace demo-agent-prod-eu
 kubectl create namespace demo-agent-staging
 kubectl create namespace demo-agent-dev
-kubectl create namespace demo-agent-prod-eu
+
 kubectl create ns demo-infra
 kubectl create ns demo-dev
 kubectl create ns demo-staging
-kubectl create ns demo-prod
+kubectl create ns demo-prod-us
 kubectl create ns demo-prod-eu
 
-kubectl -n=demo-agent-prod create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=demo-agent-prod-us create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
 kubectl -n=demo-agent-prod-eu create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
 kubectl -n=demo-agent-staging create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
 kubectl -n=demo-agent-dev create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
@@ -22,34 +27,29 @@ helm repo add prometheus-community https://prometheus-community.github.io/helm-c
 # Update repo to fetch latest armory charts
 helm repo update
 # Install or Upgrade armory rna chart
-helm upgrade --install armory-rna-prod armory/remote-network-agent \
+helm upgrade --install demo-rna-prod-us armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=demo-prod-west-cluster \
-    -n demo-agent-prod
-helm upgrade --install armory-rna-prod-eu armory/remote-network-agent \
+    --set agentIdentifier=demo-rna-prod-us-cluster \
+    -n demo-agent-prod-us
+helm upgrade --install demo-rna-prod-eu armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=demo-prod-eu-cluster \
+    --set agentIdentifier=demo-rna-prod-eu-cluster \
     -n demo-agent-prod-eu
-helm upgrade --install armory-rna-staging armory/remote-network-agent \
+helm upgrade --install demo-rna-staging armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=demo-staging-cluster \
+    --set agentIdentifier=demo-rna-staging-cluster \
     -n demo-agent-staging
-helm upgrade --install armory-rna-dev armory/remote-network-agent \
+helm upgrade --install demo-rna-dev armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=demo-dev-cluster \
+    --set agentIdentifier=demo-rna-dev-cluster \
     -n demo-agent-dev
 
 helm install prometheus prometheus-community/kube-prometheus-stack -n=demo-infra --set "kube-state-metrics.metricAnnotationsAllowList[0]=pods=[*]" --set "global.scrape_interval=5s"
 
 BASEDIR=$(dirname $0)
 
-kubectl apply -f "$BASEDIR/../manifests/potato-facts-external-service.yml" -n demo-prod-eu #Temporary workaround for YODL-300. deploying service along side deployment does not work for Blue/Green.
-#container_cpu_load_average_10s{namespace="borealis", job="kubelet"} * on (pod)  group_left (label_app) sum(kube_pod_labels{job="kube-state-metrics",label_app="hostname",namespace="borealis"}) by (label_app, pod)
-
-# also tried: --set kube-state-metrics.metricLabelsAllowlist=pods=[*]
-# k -n=borealis-demo-infra port-forward service/prometheus-kube-prometheus-prometheus 9090
-# example prometheus query for CPU load for pods in a replica set. container_cpu_load_average_10s{name=~"k8s_POD_hostname-5b8bc655f6.+"}
+kubectl apply -f "$BASEDIR/../manifests/potato-facts-external-service.yml" -n demo-prod-eu
