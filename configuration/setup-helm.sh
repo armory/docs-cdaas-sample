fbad997ce9e3c56ba1e1dff8a743c7f42f27cdf8
Aimee Ukasick <aimeeu.opensource@gmail.com>
2022-10-04 17:30:57 -0500

    fixed deploy.yml

--- configuration/setup-helm.sh
+++ configuration/setup-helm.sh
@@ -5,17 +5,22 @@ if [ "$#" -ne 2 ]; then
 fi
 
 # Create namespaces for the Remote Network Agents
-kubectl create namespace sample-rna-prod-us
-kubectl create namespace sample-rna-prod-eu
-kubectl create namespace sample-rna-staging
 kubectl create namespace sample-rna-test
+kubectl create namespace sample-rna-staging
+kubectl create namespace sample-rna-prod-eu
+kubectl create namespace sample-rna-prod-us
 
 # Create secrets in each Remote Network Agent namespace to the RNA can communicate with CD-as-a-Service
-kubectl -n=sample-rna-prod-us create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
-kubectl -n=sample-rna-prod-eu create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
-kubectl -n=sample-rna-staging create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
 kubectl -n=sample-rna-test create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=sample-rna-staging create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=sample-rna-prod-eu create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
+kubectl -n=sample-rna-prod-us create secret generic rna-client-credentials --type=string --from-literal=client-secret=$2 --from-literal=client-id=$1
 
+# Create namespaces to mimic target deployment clusters
+kubectl create ns sample-test
+kubectl create ns sample-staging
+kubectl create ns sample-prod-eu
+kubectl create ns sample-prod-us
 
 # Optionally Add Armory Chart repo, if you haven't
 helm repo add armory https://armory.jfrog.io/artifactory/charts
@@ -26,29 +31,23 @@ helm repo update
 # clusterrole and clusterrolebinding are created for each RNA in the `default` namespace
 # Because we are using multiple namespaces in a single cluster instead of the same namespace in multiple clusters, use a different release name
 # for each namespace. If you were installing one RNA per cluster, you would use the same namespace and release name.
-helm upgrade --install sample-rna-prod-us armory/remote-network-agent \
-    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
-    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=sample-rna-prod-us-cluster \
-    -n sample-rna-prod-us
-helm upgrade --install sample-rna-prod-eu armory/remote-network-agent \
-    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
-    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=sample-rna-prod-eu-cluster \
-    -n sample-rna-prod-eu
-helm upgrade --install sample-rna-staging armory/remote-network-agent \
-    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
-    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
-    --set agentIdentifier=sample-rna-staging-cluster \
-    -n sample-rna-staging
 helm upgrade --install sample-rna-test armory/remote-network-agent \
     --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
     --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
     --set agentIdentifier=sample-rna-test-cluster \
     -n sample-rna-test
-
-# Create namespaces to mimic target deployment clusters
-kubectl create ns sample-test
-kubectl create ns sample-staging
-kubectl create ns sample-prod-us
-kubectl create ns sample-prod-eu
+helm upgrade --install sample-rna-staging armory/remote-network-agent \
+    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
+    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
+    --set agentIdentifier=sample-rna-staging-cluster \
+    -n sample-rna-staging
+helm upgrade --install sample-rna-prod-eu armory/remote-network-agent \
+    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
+    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
+    --set agentIdentifier=sample-rna-prod-eu-cluster \
+    -n sample-rna-prod-eu
+helm upgrade --install sample-rna-prod-us armory/remote-network-agent \
+    --set clientId='encrypted:k8s!n:rna-client-credentials!k:client-id' \
+    --set clientSecret='encrypted:k8s!n:rna-client-credentials!k:client-secret' \
+    --set agentIdentifier=sample-rna-prod-us-cluster \
+    -n sample-rna-prod-us
